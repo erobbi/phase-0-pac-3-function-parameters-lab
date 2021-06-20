ff7558e8304dc565437d84f098ad2411b38dbc5d
erobbi <elirobbins0@gmail.com>
2021-06-20 19:16:17 -0400

    completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+var name = "Josh";
+var language = "Klingon"
+var firstName = "Eli"
+
+
+function introduction(name) {
+    return "Hi, my name is ${name}.";
+}
+
+function introductionWithLanguage(firstname , language) {
+    console.log(`Hello, ${firstName}! I speak ${language}`);
+  } 
